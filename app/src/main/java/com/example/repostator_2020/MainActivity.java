b2761d5a30ddc9cc78b3b6a8fba2765488018906
rubenjuarez1986 <rubenjuaeli@gmail.com>
2020-03-25 15:37:46 +0100

    Version inicial Repostator_2020

---
+++ app/src/main/java/com/example/repostator_2020/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.repostator_2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Intent ventanaRepostaje = new Intent(this, Repostaje.class);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(ventanaRepostaje);
+            }
+        });
+    }
+
+
+}
